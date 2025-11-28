14c2dcabd5f209953e95b694f061ad3e52c8c62c
Lucas Cresta <lucascresta10@gmail.com>
2025-11-28 11:07:25 -0300

    todo-list e contadores

---
+++ Aula 08/contadores.js
@@ -0,0 +1,18 @@
+console.log("Contador com for:");
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+console.log("\nContador com while:");
+let j = 1;
+while (j <= 10) {
+    console.log(j);
+    j++;
+}
+
+console.log("\nContador com do...while:");
+let k = 1;
+do {
+    console.log(k);
+    k++;
+} while (k <= 10);
