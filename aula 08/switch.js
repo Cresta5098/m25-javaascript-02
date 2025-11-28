e0dcafb0698e28098311d0ef3fb09b23a5f714e0
Lucas Cresta <lucascresta10@gmail.com>
2025-11-28 10:58:47 -0300

    altereções aula 08- condicionais, conjuntos e switch

--- aula 08/switch.js
+++ aula 08/switch.js
@@ -1,42 +1,28 @@
 /**
- * exemplos de uso do SWITCH no JS
+ * Exemplos de uso do switch no JS
  */
-console.log("menu de opções");
-console.log("1 - cadastro");
+console.log("Menu de opções");
+console.log("1 - Cadastro");
 console.log("2 - Consulta");
 console.log("3 - Alteração");
 console.log("4 - Exclusão");
-let op = 2;
+let op = 6;
 if (op==1){
-    console.log("opção escolhida: Cadastro!!!");
-}
-else if (op == 2)
-    {
-    console.log ("opção escolhida: Consulta!!!")
-    }
-else if (op == 2){
-    console.log ("opção escolhida: Alteração!!!")
-}
-else if (op == 2){
-    console.log ("opção escolhida: Exclusão!!!")
-}
-else if (op == 2){
-    console.log ("opção escolhida: Consulta!!!")
-}
-else {
+    console.log("escolheu Cadastro!");
+} else if (op == 2){
+    console.log("escolheu consulta!");
+} else if (op == 3){
+    console.log("escolheu Alteração!");
+} else if (op == 4){
+    console.log("escolheu Exclusão!");
+} else {
     console.log("escolheu uma opção inválida");
 }
-/**
- * usando o switch para a mesma situação
- */
- switch(op){
-    case 1: console.log ("escolheu Cadastro"); 
-    break;
-    case 2: console.log ("escolheu consulta");
-    break;
-    case 3: console.log ("escolheu Alteração");
-    break;
-    case 4: console.log ("exclusão Exclusão");
-    break;
-    default: console.log ("escolheu uma opção inválida");
- }
+//Usando o switch para a mesma situação
+switch(op){
+    case 1: console.log("escolheu Cadastro"); break;
+    case 2: console.log("escolheu Consulta"); break;
+    case 3: console.log("escolheu Alteração"); break;
+    case 4: console.log("escolheu Exclusão"); break;
+    default: console.log("escolheu uma opção inválida");
+}
